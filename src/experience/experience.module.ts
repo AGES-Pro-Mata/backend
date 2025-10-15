b078e6de4755c1b1738a9d071182891d40dc8b3e
Pedro Widholzer <pedro.widholzer@edu.pucrs.br>
2025-10-15 18:44:44 -0300

    Realizado os ajustes de persistencia de arquivos na criacao de experiencias

--- src/experience/experience.module.ts
+++ src/experience/experience.module.ts
@@ -3,10 +3,11 @@ import { AnalyticsModule } from 'src/analytics/analytics.module';
 import { AnalyticsService } from 'src/analytics/analytics.service';
 import { ExperienceController } from './experience.controller';
 import { ExperienceService } from './experience.service';
+import { StorageService } from 'src/storage/storage.service';
 
 @Module({
   imports: [AnalyticsModule],
-  providers: [ExperienceService, AnalyticsService],
+  providers: [ExperienceService, AnalyticsService, StorageService],
   controllers: [ExperienceController],
 })
 export class ExperienceModule {}
